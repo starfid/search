d40804990be3b7b5973792c57b872c31fefc7077
Starfid <50829388+starfid@users.noreply.github.com>
2020-12-21 22:32:50 +0700

    major update

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
 <?php
 	session_start();
 	require('settings.php');
-	require('pdo.php');
 
 	require('engine.php');
 	$result = new Search($settings);
 
 	require('view.php');
-	$display = new Serp(
-		$result,
-		$settings['preference']
-	);
+	$display = new Serp($result, $settings['preference']);
 
 	echo $display->data;
 
 	//echo microtime(true)-$benchmark;
-	unset($_SESSION['dictionary']);
+	//unset($_SESSION['dictionary']);
 ?>
