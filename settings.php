e95b0edb77fa7f2889e77676209776aa91d12072
Starfid <50829388+starfid@users.noreply.github.com>
2020-12-21 22:30:46 +0700

    major update

--- settings.php
+++ settings.php
@@ -9,56 +9,92 @@
 			'rdbms'		=> 'mysql',
 			'host'		=> '127.0.0.1',
 			'port'		=> '3306',
-			'username'	=> 'root',
-			'password'	=> 'password',
-			'dbname'	=> 'database name'
+			'username'	=> 'your db username',
+			'password'	=> 'your db password',
+			'dbname'	=> 'your db name'
 		),
 
 		'tables' => array(
-
 			array(
 				"name" 			=> "buku",
-				"index"			=> array("judul","penulis1"),
-				"category"		=> "case when gol_pustaka = 'BK' then 'buku' when gol_pustaka = 'SK' then 'skripsi' when gol_pustaka = 'AV' then 'multimedia' end",
+				"index"			=> array("judul","penulis1","editor"),
+				"category"		=> "case when gol_pustaka = 'BK' then 'buku' when gol_pustaka = 'SK' then 'skripsi' when gol_pustaka = 'LP' then 'laporan' when gol_pustaka = 'AV' then 'multimedia' end",
 				"header"		=> "judul",
-				"location"		=> "kelas",
-				"additional"		=> "concat(penulis1,'. ',penerbit,'. ',th_terbit,'. ', kota,'..')",
+				"location"		=> "if(gol_pustaka = 'SK',concat('Rak ',penerbit,' &gt; ',kelas),concat('Rak ',kelas))",
+				"additional"		=> "concat(penulis1,'. ',if(gol_pustaka = 'SK',concat('Bimbingan ',editor),penerbit),'. ',th_terbit,'. ', kota,'..')",
 				"entry"			=> "date(tgl_input)",
+				"year"			=> "th_terbit",
+				"limit"			=> "25",
+			),
+			array(
+				"name" 			=> "jurnal_artikel",
+				"index"			=> array("judul","penulis","artikel","nama_jurnal"),
+				"category"		=> "'jurnal'",
+				"header"		=> "judul",
+				"location"		=> "concat(nama_jurnal,' &gt; Vol ',volume,' &gt; Nomor ',nomor,' &gt; Tahun ',tahun)",
+				"additional"		=> "concat(penulis,'. ',artikel)",
+				"entry"			=> "date(tgl_input)",
+				"year"			=> "tahun",
+				"limit"			=> "25",
+			),
+			array(
+				"name" 			=> "jurnal",
+				"index"			=> array("judul","subject"),
+				"category"		=> "'ebook'",
+				"header"		=> "replace(judul,'.pdf','')",
+				"location"		=> "vendor",
+				"additional"		=> "concat(subject,'. ',tahun)",
+				"entry"			=> "tahun",
+				"year"			=> "tahun",
+				"limit"			=> "25",
+			),
+			array(
+				"name" 			=> "anggota",
+				"index"			=> array("nama","alamat"),
+				"category"		=> "'people'",
+				"header"		=> "nama",
+				"location"		=> "(select concat(fakultas,' &gt; ',jurusan) from fakultas where fakultas.kd_fakultas = anggota.kd_fakultas)",
+				"additional"		=> "alamat",
+				"entry"			=> "tgl_daftar",
+				"year"			=> "angkatan",
+				"limit"			=> "25",
+			),
+			array(
+				"name" 			=> "eprint",
+				"index"			=> array("nama","title"),
+				"category"		=> "'repository'",
+				"header"		=> "title",
+				"location"		=> "concat('http://example.org/',eprintid)",
+				"additional"		=> "abstrak",
+				"entry"			=> "date(entry)",
+				"year"			=> "tahun",
 				"limit"			=> "25",
 			),
 			
-			array(
-				"name" 			=> "jurnal_artikel",
-				"index"			=> array("judul","penulis"),
-				"category"		=> "'jurnal'",
-				"header"		=> "judul",
-				"location"		=> "kelas",
-				"additional"		=> "concat(penulis,'. ',artikel)",
-				"entry"			=> "date(tgl_input)",
-				"limit"			=> "25",
-			),
+			
 
 		),
 
 		'preference' => array(
-			'site'		=> array(
-				'name'			=> 'Lorem Ipsum Dolor Sit Amet',
-				'desc'			=> 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+			'site'			=> array(
+				'name'			=> 'Search Engine',
+				'desc'			=> 'Indexing multiple tables and sort the result in relevance order start from the top list',
 			),
-			'campaign'	=> array(
-				'title'			=> 'Excepteur Sint',
-				'desc'			=> 'Duis aute irure dolor in reprehenderit in voluptate'
+			'campaign'		=> array(
+				'title'			=> 'Protect Each Other',
+				'desc'			=> 'Love your family and friends, wear a face mask'
 			),
-
-
-			'debug'		=> true,
-			'prefix' 	=> array(
-				'buku' 			=> array('buku','definisi','arti','pengertian'),
-				'skripsi' 		=> array('skripsi','bimbingan','pdf','fakultas','prodi','jurusan'),
-				'multimedia'		=> array('kaset','cd','dvd'),
-				'jurnal'		=> array('jurnal'),
-				'ebook'			=> array('ebook')
-			)
+			'debug'			=> true,
+			'categories'		=> array(
+				'people'		=> 'Student and employee',
+				'buku' 			=> 'Collections at level 3 and 4',
+				'skripsi' 		=> 'Collections at level 2',
+				'multimedia' 		=> 'Collection at level 2',
+				'laporan' 		=> 'Collection at level 2',
+				'jurnal' 		=> 'Collection at level 2',
+				'ebook' 		=> 'Online journal subscription',
+				'repository' 		=> 'Online collections'
+			),
 		)
 
 	);
