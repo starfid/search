40f90b6ac8667fda58731ac8b606d94c41c86a0a
Starfid <50829388+starfid@users.noreply.github.com>
2020-12-22 11:39:57 +0700

    add rss format

--- view.php
+++ view.php
@@ -1,51 +1,41 @@
 <?php
-	class Serp {
-		public $data;
-		private $result, $keywords, $selectedCat, $allCats, $name, $emptyResult, $debug, $placeholder, $error;
+	class Serp extends Tool{
+		public $content, $keywords;
+		private $data, $selectedCat, $allCats, $name, $siteTitle, $emptyResult, $debug, $placeholder, $error;
 
-		function __construct($x, $preference){
-			$this->keywords = $x->keywords;
-			
+		function __construct($result, $preference){
+			$this->keywords = $result->keywords;
+
 			$this->allCats = array_keys($preference['categories']);
 			array_unshift($this->allCats,"all");
 
-			$this->selectedCat = $x->selectedCat;
+			$this->selectedCat = $result->selectedCat;
 			$this->pref = $preference;
 			$this->debug = $preference['debug'];
 
-			$this->result = $x->data;
-			$this->error = $x->error;
-			$this->emptyResult = is_null($x->data)?True:False;
+			$this->data = $result->data;
+			$this->error = $result->error;
+			$this->emptyResult = is_null($result->data)?True:False;
 			$this->emptyKeyword = (!isset($_GET['search']) || empty($_GET['search']))?True:False;
 			
 			$this->placeholder = isset($this->keywords['original'])?$this->keywords['original']['placeholder']:"";
+			
+			$this->siteTitle = !empty($this->placeholder)?ucwords($this->placeholder)." - ":"";
+			$this->siteTitle = $this->siteTitle.$this->pref['site']['name'];
 
-			$this->html();
-		}
-		function highlight($txt){
-			if(
-				!isset($this->keywords) || 
-				!array_key_exists('new',$this->keywords) || 
-				!array_key_exists('final',$this->keywords['new'])
-			) return $txt;
+			$format = isset($_GET['format'])?$_GET['format']:NULL;
+			method_exists($this,$format)?$this->$format():$this->html();
 
-			$words = $this->keywords['new']['final'];
-			array_multisort(array_map('strlen', $words), $words);
-			$txt = preg_replace("#(".implode("|",$words).")#i", "<strong>$1</strong>", $txt);
-			return $txt;
 		}
+
 		function html(){
 			$s = "<!DOCTYPE html>";
 			$s .= "\n<html lang='en'>";
 			$s .= "\n\t<head>";
-
-			$header = !empty($this->placeholder)?ucwords($this->placeholder)." - ":"";
-			$header = $header.$this->pref['site']['name'];
-
-			$s .= "\n\t\t<title>".$header."</title>";
+			$s .= "\n\t\t<title>".$this->siteTitle."</title>";
 			$s .= "\n\t\t<meta content=\"yes\" name=\"apple-mobile-web-app-capable\" />";
 			$s .= "\n\t\t<meta content=\"notranslate\" name=\"google\" />";
-			$s .= "\n\t\t<meta name=\"theme-color\" content=\"#5D5D5D\" />";
+			$s .= "\n\t\t<meta content=\"#5D5D5D\" name=\"theme-color\" />";
 			$s .= "\n\t\t<meta content=\"".$this->pref['site']['desc']."\" name=\"description\" />";
 			$s .= "\n\t\t<meta content=\"width=device-width,initial-scale=1,shrink-to-fit=no\" name=\"viewport\" />";
 			$s .= "\n\t\t<meta content=\"text/html;charset=utf-8\" http-equiv=\"Content-Type\" />";
@@ -86,7 +76,6 @@
 			if(!$this->emptyKeyword){
 				$s .= "\n\t\t<div id=\"catswrap\">";
 				$s .= "\n\t\t\t<ul id=\"cats\" class=\"mid970\">";
-
 				
 				foreach($this->allCats as $category){
 					$borderBottom = $category==$this->selectedCat?" class=\"selCat\"":"";
@@ -97,14 +86,21 @@
 				$s .= "\n\t\t</div>";
 			}
 		
-			
 			$s .= "\n\t\t<div id=\"content\" class=\"float mid1000\">";
 
 			$s .= "\n\t\t\t<div class=\"left\" id=\"results\">";
 
 			if(count($this->error)>0){
-				$s .= "\n\t\t\t\t<dl>\n\t\t\t\t\t<dt>Check your SQL in table settings</dt><dd>".implode('<br />',$this->error)."</dd></dl>";
+				$s .= "\n\t\t\t\t<dl>\n\t\t\t\t\t<dt class=\"error\">Check your SQL</dt>";
+				$s .= "\n\t\t\t\t\t<dd>";
+				$s .= "\n\t\t\t\t\t\t<div class=\"loc\">Settings.php &gt; Tables</div>";
+				$s .= "\n\t\t\t\t\t\t<div class=\"info\">".implode('<br />',$this->error)."</div>";
+				$s .= "\n\t\t\t\t\t</dd>";
+				$s .= "\n\t\t\t\t</dl>";
 			}
+
+			
+
 			elseif($this->emptyResult){
 				$s .= "\n\t\t\t\t<dl>\n\t\t\t\t\t<dd>";
 				if(!empty($this->placeholder)){
@@ -122,10 +118,10 @@
 				$s .= "\n\t\t\t\t</dl>";
 			}
 			else{
-				for($i=0;$i<count($this->result);$i++){
-					$data = $this->result[$i];
+				for($i=0;$i<count($this->data);$i++){
+					$data = $this->data[$i];
 					
-					$title = in_array('rank',$data)&&$this->debug?" title=\"".$data['rank']."\"":"";
+					$title = !$this->emptyKeyword && $this->debug?" title=\"".$data['rank']."\"":"";
 
 					$s .= "\n\t\t\t\t<dl".$title.">";
 					$s .= "\n\t\t\t\t\t<dt>".$this->highlight(stripslashes(stripslashes($data['header'])))."</dt>";
@@ -142,14 +138,51 @@
 
 			$s .= "\n\t<div>x</div>";
 
-			$this->data = $s;
+			$this->content = $s;
 
 		}
-		function xml(){
 
+		function rss(){
+			header('Content-Type: text/xml;charset=UTF-8');
+			$s = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+			$s .= "\n<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\" xmlns:content=\"http://purl.org/rss/1.0/modules/content/\" xmlns:media=\"http://search.yahoo.com/mrss/\">";
+			$s .= "\n\t<channel>";
+			$s .= "\n\t\t<lastBuildDate>".date("D, d M Y H:i:s T")."</lastBuildDate>";
+			$s .= "\n\t\t<title>".$this->siteTitle."</title>";
+			$s .= "\n\t\t<description>".$this->pref['site']['desc']."</description>";
+			
+			
+			for($i=0;$i<count($this->data);$i++){
+				$data = $this->data[$i];
+
+				$s .= "\n\t\t<item>";
+				$s .= "\n\t\t\t<pubdate>".date("D, d M Y H:i:s",strtotime($data['entry']))."</pubdate>";
+				$s .= "\n\t\t\t<title>".stripslashes(stripslashes($data['header']))."</title>";
+				$s .= "\n\t\t\t<guid>".ucfirst($data['category'])." > ".html_entity_decode(ucfirst($data['location']))."</guid>";
+				$s .= "\n\t\t\t<description>".stripslashes($data['additional'])."</description>";
+				$s .= "\n\t\t</item>";
+			}
+			$s .= "\n\t</channel>";
+			$s .= "\n</rss>";
+			
+			$this->content = $s;
 		}
-		function json(){
 
+	}
+
+	class Tool {
+		function highlight($txt){
+			if(
+				!isset($this->keywords) || 
+				!array_key_exists('new',$this->keywords) || 
+				!array_key_exists('final',$this->keywords['new'])
+			) return $txt;
+
+			$words = $this->keywords['new']['final'];
+			array_multisort(array_map('strlen', $words), $words);
+			$txt = preg_replace("#(".implode("|",$words).")#i", "<strong>$1</strong>", $txt);
+			return $txt;
 		}
 	}
+
 ?>
