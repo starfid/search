8171d49ffe2274794efb969ce039700e3880ec51
Starfid <50829388+starfid@users.noreply.github.com>
2021-03-11 06:45:27 +0700

    sample with multiple words

--- dictionary/similarity.php
+++ dictionary/similarity.php
@@ -7,5 +7,16 @@
 		'abott' => 'abbott',
 		'accelereted' => 'accelerated',
 		'aguste' => 'auguste',
+		'enterpreneur' => 'entrepreneur',
+		'tionghoa' => 'cina',
+		'perkawinan' => 'pernikahan,nikah,kawin',
+		'pernikahan' => 'perkawinan,nikah,kawin',
+		'metopen' => 'penelitian',
+		'metode' => 'metodologi',
+		'metodologi' => 'metode',
+		'hijab' => 'jilbab,niqab'
+		'jilbab' => 'hijab',
+		'jalaludin' => 'jalaluddin',
+		'kristen' => 'nasrani,christian',
 	);
 ?>
